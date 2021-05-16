22cce0bd1e4a8adfb94e6b02d57d970aeb42c8a4
Luca Deluigi <lucadelu97@gmail.com>
2021-05-16 02:50:23 +0200

    Complete single file conversion

--- Dockerfile
+++ Dockerfile
@@ -10,9 +10,7 @@ RUN mkdir -p /usr/share/man/man1 && apk add -q openjdk11 maven graphviz
 ENV ALLOW_PLANTUML_INCLUDE=true
 RUN wget -q -O plantuml.jar https://nav.dl.sourceforge.net/project/plantuml/plantuml.jar
 RUN mkdir -p /opt/plantuml && mv plantuml.jar /opt/plantuml/plantuml.jar
-COPY plantuml.sh /usr/local/bin/plantuml
-RUN mkdir /wiki
-ENV PLANTUML_JAVAOPTS="-Dplantuml.include.path=/wiki"
+COPY plantuml.sh /usr/local/bin/plantuml 
 
 # Configure Python Markdown
 COPY pymd_config.yml /pymd_config.yml
@@ -20,3 +18,6 @@ COPY pymd_config.yml /pymd_config.yml
 COPY start.sh /start.sh
 RUN chmod +x /start.sh
 ENTRYPOINT [ "/start.sh" ]
+
+# DEBUG ONLY
+RUN mkdir /asd && touch /asd/start.md
--- start.sh
+++ start.sh
@@ -1,5 +1,58 @@
 #!/bin/sh
-#cp -R $2 /wiki
-echo "# Test\n" >> src.md
-markdown_py -x plantuml_markdown -c /pymd_config.yml src.md > out.html
-cat out.html
+SRC=$1
+DEST=${2%/}
+echo "Source: ${SRC}; Destination: ${DEST}/"
+
+allowed_format() {
+    grep -F -q -x ".${1#.}" <<EOF
+.markdown
+.mdown
+.mkdn
+.md
+.mkd
+.mdwn
+.mdtxt
+.mdtext
+.text
+.Rmd
+EOF
+}
+
+
+if [ ! -d "${DEST}/" ] ; then
+    >&2 echo "[ERROR] ${DEST} is not valid destination path"
+    exit 1
+fi
+
+if [ -d "${SRC}" ] ; then
+    export PLANTUML_JAVAOPTS="-Dplantuml.include.path=${SRC}"
+    (cd ${SRC} ; find -type d -links 2 -exec mkdir -p "${DEST}/{}" \;)
+    find ${SRC} -type f ! -path . ! -path ./.git ! -path ./.github -exec echo {} \;
+        # OUTPUT=$DEST/${FOLDER:2}
+        # mkdir -p "$OUTPUT"
+        # FILENAME="${SRC%.*}"
+        # RESULTNAME="${FILENAME}.html"
+        # echo "Converting ${FILENAME} to ${RESULTNAME}..."
+        # markdown_py -x plantuml_markdown -c /pymd_config.yml "${SRC}" > $2/${RESULTNAME}
+        # DOCS+=( "${FOLDER:2}" )
+    
+else
+    if [ -f "${SRC}" ]; then
+        FILENAME="${SRC%.*}"
+        FILENAME="${FILENAME#/}"
+        FORMAT="${SRC#*.}"
+        if allowed_format "$FORMAT" ; then
+            RESULTNAME="${FILENAME##*/}.html"
+            echo "Converting ${SRC} to $DEST/${RESULTNAME}..."
+            markdown_py -x plantuml_markdown -c /pymd_config.yml "${SRC}" > $DEST/${RESULTNAME}
+        else
+            echo $FORMAT
+            cp -f "$SRC" "$DEST/"
+        fi
+    else
+        >&2 echo "[ERROR] ${SRC} is not valid source";
+        exit 1
+    fi
+fi
+
+ls $DEST
