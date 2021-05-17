aacf9588f8621e54d406266d6e397b98ff9de32c
ldeluigi <ldeluigi@users.noreply.github.com>
2021-05-17 22:48:09 +0000

    deploy: 3a72c3a1312e33d0fc0aa3b827918c8499e2aa5b

--- try_convert.sh
+++ try_convert.sh
@@ -3,10 +3,21 @@ set -e
 SRC="${1%/}/"
 FILE="${2#/}"
 DEST="${3%/}/"
+
+# Temp directory
+TMP_DIR=/tmp/try_convert/
+mkdir -p "$TMP_DIR"
+
 if [ -f "${SRC}${FILE}" ]; then
     BASENAME="${FILE%%.*}"
     FORMAT="${FILE#*.}"
     if allowed_format.sh "$FORMAT" ; then
+        # Add TOC if present as fourth arg
+        if [ -f "$4" -a ! "${FILE}" = "contents.md" ] ; then
+            TMP_FILE="${FILE##*/}"
+            cat "$4" "${SRC}${FILE}" > "${TMP_DIR}${TMP_FILE}"
+            mv "${TMP_DIR}${TMP_FILE}" "${SRC}${FILE}"
+        fi
         RESULTNAME="${BASENAME}.html"
         echo "Converting ${SRC}${FILE} to ${DEST}${RESULTNAME}..."
         # sed 's/\[\[(.*?)\.(markdown|mdown|mkdn|md|mkd|mdwn|mdtxt|mdtext|text)\]\]/[[\1.html]]/g' | sed 's/\[(.*)\]\((.*)\.(markdown|mdown|mkdn|md|mkd|mdwn|mdtxt|mdtext|text)\)/[\1](\2.html)/g' < "${SRC}${FILE}" > "${SRC}${FILE}"
