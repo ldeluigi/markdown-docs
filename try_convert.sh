682df70f8dc70c866b0fde2438a7c60071f72fa6
Luca Deluigi <lucadelu97@gmail.com>
2021-05-17 21:51:15 +0200

    Working on link replacement..

--- try_convert.sh
+++ try_convert.sh
@@ -9,7 +9,7 @@ if [ -f "${SRC}${FILE}" ]; then
     if allowed_format.sh "$FORMAT" ; then
         RESULTNAME="${BASENAME}.html"
         echo "Converting ${SRC}${FILE} to ${DEST}${RESULTNAME}..."
-        sed 's/\[\[(.*?)\.(markdown|mdown|mkdn|md|mkd|mdwn|mdtxt|mdtext|text)\]\]/[[\1.html]]/g' | sed 's/\[(.*)\]\((.*)\.(markdown|mdown|mkdn|md|mkd|mdwn|mdtxt|mdtext|text)\)/[\1](\2.html)/g' < "${SRC}${FILE}" > "${SRC}${FILE}"
+        # sed 's/\[\[(.*?)\.(markdown|mdown|mkdn|md|mkd|mdwn|mdtxt|mdtext|text)\]\]/[[\1.html]]/g' | sed 's/\[(.*)\]\((.*)\.(markdown|mdown|mkdn|md|mkd|mdwn|mdtxt|mdtext|text)\)/[\1](\2.html)/g' < "${SRC}${FILE}" > "${SRC}${FILE}"
         markdown_py -x plantuml_markdown -c /pymd_config.yml "${SRC}${FILE}" > "${DEST}${RESULTNAME}"
     else
         cp -f "${SRC}${FILE}" "${DEST}${FILE}"
