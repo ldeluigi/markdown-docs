68cb6912f4bfa4d31f98fc23282086dd1f2d8290
ldeluigi <ldeluigi@users.noreply.github.com>
2021-05-17 23:38:45 +0000

    deploy: bf463e9275f153e732aa7591a0873e6137bb8ec5

--- try_convert.sh
+++ try_convert.sh
@@ -12,16 +12,20 @@ if [ -f "${SRC}${FILE}" ]; then
     BASENAME="${FILE%%.*}"
     FORMAT="${FILE#*.}"
     if allowed_format.sh "$FORMAT" ; then
+        TMP_FILE="$(cat /proc/sys/kernel/random/uuid)"
         # Add TOC if present as fourth arg
         if [ -f "$4" -a ! "${FILE}" = "contents.md" ] ; then
-            TMP_FILE="$(cat /proc/sys/kernel/random/uuid)"
             cat "$4" "${SRC}${FILE}" > "${TMP_DIR}${TMP_FILE}"
-            mv "${TMP_DIR}${TMP_FILE}" "${SRC}${FILE}"
+        else
+            cp "${SRC}${FILE}" "${TMP_DIR}${TMP_FILE}"
         fi
         RESULTNAME="${BASENAME}.html"
         echo "Converting ${SRC}${FILE} to ${DEST}${RESULTNAME}..."
-        # sed 's/\[\[(.*?)\.(markdown|mdown|mkdn|md|mkd|mdwn|mdtxt|mdtext|text)\]\]/[[\1.html]]/g' | sed 's/\[(.*)\]\((.*)\.(markdown|mdown|mkdn|md|mkd|mdwn|mdtxt|mdtext|text)\)/[\1](\2.html)/g' < "${SRC}${FILE}" > "${SRC}${FILE}"
-        markdown_py -x plantuml_markdown -c /pymd_config.yml "${SRC}${FILE}" > "${DEST}${RESULTNAME}"
+        cat "${TMP_DIR}${TMP_FILE}"
+        sed -i -r 's/\[\[(.*?)\.(markdown|mdown|mkdn|md|mkd|mdwn|mdtxt|mdtext|text)\]\]/[[\1.html]]/g' "${TMP_DIR}${TMP_FILE}"
+        sed -i -r 's/\[(.*)\]\((.*)\.(markdown|mdown|mkdn|md|mkd|mdwn|mdtxt|mdtext|text)\)/[\1](\2.html)/g' "${TMP_DIR}${TMP_FILE}"
+        cat "${TMP_DIR}${TMP_FILE}"
+        markdown_py -x plantuml_markdown -c /pymd_config.yml "${TMP_DIR}${TMP_FILE}" > "${DEST}${RESULTNAME}"
     else
         cp -f "${SRC}${FILE}" "${DEST}${FILE}"
     fi
