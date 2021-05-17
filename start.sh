ba47da40774634678d48960d5d9793ca3b2e59e0
Luca Deluigi <lucadelu97@gmail.com>
2021-05-17 16:37:28 +0200

    Add mode parameters

--- start.sh
+++ start.sh
@@ -1,27 +1,34 @@
 #!/bin/sh
-SRC=${1#.}
-SRC=${GITHUB_WORKSPACE%/}/${SRC#/}
-DEST=${2#.}
-DEST=${GITHUB_WORKSPACE%/}/${DEST#/}
-DEST=${DEST%/}
-echo "Source: ${SRC}; Destination: ${DEST}/"
+MODE="$1"
+if [ "$MODE" = "HTML" -o "$MODE" = "html" -o "$MODE" = "gh-pages" ] ; then
+    SRC=${2#.}
+    SRC=${GITHUB_WORKSPACE%/}/${SRC#/}
+    DEST=${3#.}
+    DEST=${GITHUB_WORKSPACE%/}/${DEST#/}
+    DEST=${DEST%/}
+    echo "Source: ${SRC}; Destination: ${DEST}/"
 
-if [ ! -d "${DEST}/" ] ; then
-    >&2 echo "[ERROR] ${DEST} is not valid destination path"
-    exit 1
-fi
-
-if [ -d "${SRC}" ] ; then
-    export PLANTUML_JAVAOPTS="-Dplantuml.include.path=${SRC}"
-    (cd "${SRC}" ; find * -type d ! -path .git ! -path .github -exec mkdir -p ${DEST}/{} \;)
-    (cd "${SRC}" ; find * -type f ! -path .git ! -path .github -exec try_convert.sh "${SRC}" "{}" "${DEST}/" \;)
-else
-    if [ -f "${SRC}" ]; then
-        try_convert.sh "${SRC%/*}/" "${SRC##*/}" "${DEST}/"
-    else
-        >&2 echo "[ERROR] ${SRC} is not valid source";
+    if [ ! -d "${DEST}/" ] ; then
+        >&2 echo "[ERROR] ${DEST} is not valid destination path"
         exit 1
     fi
+
+    if [ -d "${SRC}" ] ; then
+        export PLANTUML_JAVAOPTS="-Dplantuml.include.path=${SRC}"
+        (cd "${SRC}" ; find * -type d ! -path .git ! -path .github -exec mkdir -p ${DEST}/{} \;)
+        (cd "${SRC}" ; find * -type f ! -path .git ! -path .github -exec try_convert.sh "${SRC}" "{}" "${DEST}/" \;)
+    else
+        if [ -f "${SRC}" ]; then
+            try_convert.sh "${SRC%/*}/" "${SRC##*/}" "${DEST}/"
+        else
+            >&2 echo "[ERROR] ${SRC} is not valid source";
+            exit 1
+        fi
+    fi
+    echo "Done. Result:"
+    tree $DEST
+elif [ "$MODE" = "Markdown" -o "$MODE" = "markdown" -o "$MODE" = "md" ] ; then
+    >&2 echo "[ERROR] ${MODE} is currently not supported";
+else
+    >&2 echo "[ERROR] ${MODE} is not a valide mode";
 fi
-echo "Done. Result:"
-tree $DEST
