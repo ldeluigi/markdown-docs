9d451b317de1334eb71738c88293c5b950e57781
Luca Deluigi <lucadelu97@gmail.com>
2021-05-28 17:43:07 +0200

    Remove mode arg + disable qrcode (not working)

--- script/makedocs.sh
+++ script/makedocs.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 set -e
-MODE="$1"
 WORKSPACE="${WORKSPACE:-${GITHUB_WORKSPACE:-/}}"
 export REPOSITORY="${GITHUB_REPOSITORY:-Documentation}"
 if [ -z "${GITHUB_SERVER_URL}" -a -z "${GITHUB_REPOSITORY}" ] ; then
     export GIT=1
     export REPO_URL="${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}"
 fi
-SRC=${2#.}
+SRC=${1#.}
 export SRC=${WORKSPACE%/}/${SRC#/}
 export PLANTUML_JAVAOPTS="-Dplantuml.include.path=${SRC}"
-RELATIVE_DST=${3#.}
+RELATIVE_DST=${2#.}
 RELATIVE_DST=${RELATIVE_DST#/}
 RELATIVE_DST=${RELATIVE_DST%/}
 DST=${WORKSPACE%/}/${RELATIVE_DST}
@@ -18,6 +17,6 @@ TMP_DST=/tmp/makedocs
 echo "Source: ${SRC}; Destination: ${DST}"
 
 #xvfb-run -a mkdocs build -c -f /usr/local/src/mkdocs.yml -d "${DST}"
-rm -r "${DST}"
+rm -rf "${DST}"
 mkdocs build -c -f /usr/local/src/mkdocs.yml -d "${TMP_DST}"
 cp -r "${TMP_DST}" "${DST}"
