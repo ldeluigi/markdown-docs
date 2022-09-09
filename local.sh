ce8220e342c502c20b352d6b27218a0a77d2004a
ldeluigi <lucadelu97@gmail.com>
2022-09-09 10:32:34 +0200

    Add local test script

---
+++ local.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$1" -o -z "$2" ]; then
+    echo "Usage: $0 <path to a src folder> <path to a dest folder>"
+    exit 1
+fi
+
+docker build -t mdocs-local .
+docker run --rm -e WORKSPACE=/github/workspace -v "$(pwd):/github/workspace" mdocs-local "${1%/}" "${2%/}"
